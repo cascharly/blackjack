cde3acb12a08937d6010f415d91edf8703e39787
cascharly <cascharly@gmail.com>
2021-01-15 18:20:42 -0300

    08 Valor de cada carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -34,4 +34,23 @@ const pedirCarta = () => {
   return carta;
 };
 
-pedirCarta();
+// pedirCarta();
+
+const valorCarta = (carta) => {
+  const valor = carta.substring(0, carta.length - 1);
+
+  return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
+
+  //   let puntos = 0;
+  //   //   console.log({ valor });
+  //   if (isNaN(valor)) {
+  //         puntos = ( valor ==='A') ? 11 : 10
+  //   } else {
+  //     puntos = valor * 1;
+  //   }
+
+  //   console.log(puntos);
+};
+
+const valor = valorCarta(pedirCarta());
+console.log({valor});
